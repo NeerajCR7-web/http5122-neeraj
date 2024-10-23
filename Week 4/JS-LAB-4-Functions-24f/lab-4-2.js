097f5ca76b32995463a7006436635fa165195fd0
Neeraj Kumar <neekum73@gmail.com>
2024-10-23 17:29:02 -0400

    Week 4 files

---
+++ Week 4/JS-LAB-4-Functions-24f/lab-4-2.js
@@ -0,0 +1,26 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 2:  AN AVERAGE FUNCTION
+function avgvalue(num1, num2, num3, num4, num5){
+   
+//################## CREATE YOUR AVERAGE FUNCTION
+//This function takes five numbers and returns their average to one decimal place.
+var add = (num1 + num2 + num3 + num4 + num5)/5; 
+return add.toFixed(1);
+}
+
+//################## LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+avgvalue(5, 10, 15, 20, 25);
+console.log( avgvalue(5, 10, 15, 20, 25));
+
+var webdes = 90;
+var frontend = 90;
+var database = 70;
+var workshop = 90;
+var interaction = 85;
+
+var avggrade = avgvalue(webdes, frontend, database, workshop, interaction);
+if(avggrade > 70){
+   alert("Great, you have been graduated");
+} else {
+   alert("Review Required");
+}
---
+++ Week 4/JS-LAB-4-Functions-24f/lab-4-3.js
@@ -0,0 +1,30 @@
+﻿//#### LAB 4 - FUNCTIONS ####
+//PART 3:  SAFE DOG WALKING CHECK 
+
+
+
+//================== CREATE YOUR checkTemp FUNCTION
+//This function's job is to check the temperature and alert the user if the temperature is not in the range mentioned...
+function checkTemp(currentTemp)
+{
+    if(currentTemp > 30 || currentTemp < -10)
+        {
+        return false;
+    } 
+    else 
+    {
+        return true;
+    }
+}
+//It needs to receive the input from the user...
+var usertemp = prompt("What is the current temperature?");
+//It will return the value entered by the user...
+var newtemp = parseFloat(usertemp);
+
+//================== LOGIC THAT OUTPUTS MESSAGES BASED ON FUNCTION RESULTS
+// the value entered by the user is sent into the function and again follows the checkTemp code
+if(checkTemp(newtemp) === false){
+    alert("Yikes! No weather  for dog walking");
+} else {
+    alert("You're good, have a nice walk!");
+}
