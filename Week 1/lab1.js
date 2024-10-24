ef77dba85e68126bf54cdf172d17efbfd9a0f536
Neeraj Kumar <neekum73@gmail.com>
2024-10-24 14:22:23 -0400

    Week 1 files

---
+++ Week 1/lab1.js
@@ -0,0 +1,7 @@
+//alert("Hello World")
+var myName = "Neeraj";
+var myAge = 23;
+var daysinYear = 365;
+var mydays = myAge * daysinYear;
+var msg = myName + " is " + mydays + " years old more or less";
+alert(msg);
